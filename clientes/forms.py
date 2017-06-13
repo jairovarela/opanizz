89ddd067cd2734538cbad6f4b5740bfd23de6b59
Marcos Castellanos <makastin14@gmail.com>
2017-06-13 16:36:23 -0400

    agegado form para manejo de formularios instanciados

---
+++ clientes/forms.py
@@ -0,0 +1,45 @@
+from django import forms 
+from django.forms import ModelForm, CheckboxInput
+from models import Potenciales, Actividad
+from django import forms 
+from django.forms import ModelForm, CheckboxInput
+from django_select2.forms import Select2Widget
+from suit.widgets import SuitDateWidget, AutosizedTextarea, SuitTimeWidget, SuitSplitDateTimeWidget
+
+
+# FORMULARIOS MODIFICADOS DE LOS MODELOS 
+
+class ActividadSelect2WidgetForm(forms.ModelForm):
+	class Meta:
+		model = Actividad
+		exclude = []
+		field = ('cliente_registro',)
+		widgets = {
+		'fecha_contacto': SuitSplitDateTimeWidget,
+		'accion_registrada': AutosizedTextarea(attrs={'rows': 4, 'class': 'input-xlarge'}),
+		'fecha_prox_contacto': SuitSplitDateTimeWidget,
+		'cliente_registro': Select2Widget,
+		'volver_contactar': CheckboxInput,
+		}
+
+class PotencialesForm(forms.ModelForm):
+	class Meta:
+		model = Potenciales
+		exclude = []
+		fields =[
+			"usuario",
+			"nombre",
+			"apellido",
+			"medio_contactado",
+			"celular",
+			"telefono_h",
+			"personas",
+			"cantidad_p",
+			"adultos",
+			"valencia",
+			"zona"
+		]
+		widgets = {
+		'adultos': CheckboxInput
+		}
+
