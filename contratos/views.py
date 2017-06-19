e2b9861994bcd0df941f28890e9e292a30acb907
Marcos Castellanos <makastin14@gmail.com>
2017-06-19 07:49:09 -0400

    views contratos

--- contratos/views.py
+++ contratos/views.py
@@ -1,3 +1,16 @@
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from administracion.models import Servicios
+from forms import ContratosForm
 
-# Create your views here.
+
+def datos(request):
+    form = ContratosForm(request.POST or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.usuario = request.user    
+        instance.save()
+    return render(request, "contratos/contrato.html", {"form":form})
