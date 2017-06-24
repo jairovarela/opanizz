8bc6d10be0ecc60930bb71f139dbd5d3037f9bd0
Marcos Castellanos <makastin14@gmail.com>
2017-06-23 23:25:49 -0400

    validado registro, inicio sesion y llenado de datos como ciclo

--- clientes/views.py
+++ clientes/views.py
@@ -9,11 +9,46 @@ from contratos.forms import ContratosForm
 from django.views.generic.edit import FormView, UpdateView
 from django.views.generic import TemplateView
 from models import Potenciales
-from django.http import HttpResponseRedirect
+from django.shortcuts import redirect
 
+
+
+
+def inicio(request):
+    return render(request, "inicio.html", {})
+
+def usuario(request):
+    return render(request, "clientes/user.html", {})
+
+def handler404(request):
+    response = render_to_response('404.html', {},context_instance=RequestContext(request))
+    response.status_code = 404
+    return response
+
+def contrato(request):
+    form = ContratosForm(request.POST or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+    return render(request, "clientes/contrato.html", {"form":form})
+
+def contratados(request):
+    return render(request, "clientes/contrato_hecho.html", {})
 
 def is_profile_updated(user):
-    return all((getattr(user.potenciales, field.name) for field in user.potenciales._meta.fields))
+    if hasattr(user, 'potenciales'):
+        return all((getattr(user.potenciales, field.name) for field in user.potenciales._meta.fields))
+    return False
+
+
+
+
+class IniciarSesion(LoginRequiredMixin, generic.View):
+    template_name = 'clientes/perfil.html'
+    def get(self, request, *args, **kwargs):
+        if not is_profile_updated(request.user):
+            return redirect('/accounts/datos/')
+        return render(request, self.template_name)
 
 
 class ClienteServiciosView(LoginRequiredMixin, generic.View):
@@ -65,12 +100,6 @@ class DatosClientesView(LoginRequiredMixin, FormView):
         return super(DatosClientesView, self).post(form)
 
 
-class IniciarSesion(LoginRequiredMixin, TemplateView):
-    template_name = 'clientes/perfil.html'
-
-
-
-
 class DatosClientesUpdate(UpdateView):
     template_name = 'clientes/user.html'
     model = Potenciales
@@ -88,44 +117,6 @@ class DatosClientesUpdate(UpdateView):
 
         return super(DatosClientesUpdate, self).post(form)
 
-    
-def handler404(request):
-    response = render_to_response('404.html', {},context_instance=RequestContext(request))
-    response.status_code = 404
-    return response
 
-
-def inicio(request):
-	return render(request, "inicio.html", {})
-
-
-#def perfil(request):
-#	return render(request, "clientes/perfil.html", {})
-
-
-def usuario(request):
-    return render(request, "clientes/user.html", {})
-
-
-#def datos(request):
-#    form = PotencialesForm(request.POST or None)
-#    if form.is_valid():
-#        instance = form.save(commit=False)
-#        print form
-#        instance.usuario = request.user    
-#        instance.save()
-#    return render(request, "clientes/datos.html", {"form":form})
-
-
-def contrato(request):
-    form = ContratosForm(request.POST or None)
-    if form.is_valid():
-        instance = form.save(commit=False)
-        instance.save()
-    return render(request, "clientes/contrato.html", {"form":form})
-
-
-def contratados(request):
-    return render(request, "clientes/contrato_hecho.html", {})
 
 
